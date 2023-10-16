081c8443e9bae2323b0a458d081b65cf5fcbd5ea
juan penk <147056403+maramuraDev@users.noreply.github.com>
2023-10-16 10:48:44 +0200

    Initial commit

---
+++ a-frame-starter/README.md
@@ -0,0 +1,4 @@
+# A-Frame starter
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/NJStudio/pen/abvMorv](https://codepen.io/NJStudio/pen/abvMorv).
+
---
+++ a-frame-starter/dist/script.js
@@ -0,0 +1,16 @@
+/*
+NJ Namju Lee
+Email: nj.namju@gmail.com
+Website: www.njstudio.co.kr
+Linkedin: https://www.linkedin.com/in/nj-namju-lee-926b3252/
+
+Materials(Eng)
+youtube(eng): https://www.youtube.com/channel/UCP4q3a4ogJN1-SbJclJR3Ww
+Medium: https://medium.com/@nj.namju
+
+Materials(KR)
+youtube(kr): https://www.youtube.com/channel/UC3Z42uoe9C7Et39h5cLK1dw?view_as=subscriber
+Daum Brunch: https://brunch.co.kr/@njnamju
+  Data & Design Computaional Design : https://brunch.co.kr/@njnamju/64
+Naver Blog: https://blog.naver.com/designju
+*/
